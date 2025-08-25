4c3aa62b3d6170854aae86211953260eb45ae3ba
avoloder9 <adnan.voloder9@gmail.com>
2025-08-25 11:14:04 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./component/Header";
+import Meals from "./component/Meals";
+function App() {
+  return (
+    <>
+      <Header />
+      <Meals />
+    </>
+  );
+}
+
+export default App;
